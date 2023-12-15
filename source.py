b26cf5d33dc3f118ce4fe45117c9d49ff3bf0392
JevgenijsKostjucenko <144878198+JevgenijsKostjucenko@users.noreply.github.com>
2023-12-15 11:06:26 +0000

    15.12

--- source.py
+++ source.py
@@ -9,6 +9,7 @@ service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
+
 name=[]
 # program read information from people.csv file and put all data in name list.
 with open("people.csv", "r") as file:
@@ -16,7 +17,18 @@ with open("people.csv", "r") as file:
     for line in file:
         row=line.rstrip().split(",") 
         name.append(row)
+all_names = " ".join([row[2] for row in name])
+
 
 url = "https://emn178.github.io/online-tools/crc32.html"
 driver.get(url)
 time.sleep(2)
+
+find = driver.find_element(By.ID, "input")
+find.send_keys(all_names)
+
+find=driver.find_element(By.ID, "output")
+temp = find.get_attribute("value")
+print(temp)
+print(all_names)
+input()
